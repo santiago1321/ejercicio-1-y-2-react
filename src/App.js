fe7405199ade74e763edc07e2001de6c4c424ad0
Xamena Santiago <xamenasantiago@hotmail.com>
2022-10-02 22:22:26 -0300

    hice el maquetado

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Message from "./components/Mensaje";
+
+function App() {
+  return (
+    <>
+      <section className="container">
+        <Message></Message>
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Mensaje.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Messaje = () => {
+    return (
+        <h1>
+            hola mundo
+        </h1>
+    );
+};
+
+export default Messaje;
